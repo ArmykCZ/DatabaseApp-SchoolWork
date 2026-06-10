2c21cfba7aa0a3405eca2294d3364c4a75930698
jarosm24 <JarosM24@sosehl.onmicrosoft.com>
2026-06-10 14:22:53 +0200

    oprava vypisu okresů

--- vypis_okresu.py
+++ vypis_okresu.py
@@ -4,4 +4,4 @@ def vypis_okresu():
     cur.execute("SELECT * FROM obce_pob")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        print(f"""ID okresu: {row[0]}, ID obce: {row[1]}, název: {row[2]}, počet obyvatel: {row[3]}, počet můžů: {row[4]}, počet žen: {row[5]}, průměrný věk: {row[6]}, průměrný věk mužů: {row[7]}, průměrný věk žen: {row[8]}""")
